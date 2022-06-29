69c7f2f9014659e3719bf585b431fa8439585145
TranTam <tamvaa1306@gmail.com>
2022-06-29 09:55:31 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <search-bar />
+    <product-list />
+  </div>
+</template>
+
+<script>
+import SearchBar from "./components/SearchBar.vue";
+import ProductList from "./components/ProductList.vue";
+export default {
+  name: "App",
+  components: { SearchBar, ProductList },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="searchbar">
+    <h1>Search Bar</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
